1c6e043644683cfa4625779e3ea4c8625f4c642c
Your <ckiroshan@gmail.com>
2024-05-31 18:02:31 +0530

    Intialized the Toggle Color project.

---
+++ 5. Toggle Color Project/src/App.js
@@ -0,0 +1,11 @@
+import ToggleButtonColor from "./ToggleButtonColor";
+
+function App() {
+  return (
+    <>
+      <ToggleButtonColor />
+    </>
+  );
+}
+
+export default App;
---
+++ 5. Toggle Color Project/src/ToggleButtonColor.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const Main = () => {
+  return (
+    <>
+      <h1 className="heading">Toggle Color App</h1>
+    </>
+  );
+};
+
+export default Main;
