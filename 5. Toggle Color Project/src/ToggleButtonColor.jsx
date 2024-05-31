45ac9df1746cd1eaf6b743b10e0d91e9340b7d13
Your <ckiroshan@gmail.com>
2024-05-31 19:38:26 +0530

    Created variables to manage state with useState hooks.

--- 5. Toggle Color Project/src/ToggleButtonColor.jsx
+++ 5. Toggle Color Project/src/ToggleButtonColor.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 
 const Main = () => {
+  const [backgroundColor, setBackgroundColor] = useState("white");
+  const [textColor, setTextColor] = useState("#1b1b1b");
+  const [buttonStyle, setButtonStyle] = useState("white");
+  const [headingText, setHeadingText] = useState("Welcome To The \n Real World \n 😇");
   return (
     <>
       <h1 className="heading">Toggle Color App</h1>
