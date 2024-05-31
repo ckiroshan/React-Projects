7b8c2a6c6b997684fd2a15f0e653f12c3476046d
Your <ckiroshan@gmail.com>
2024-05-31 19:41:53 +0530

    Implemented heading to change dynamically upon each click.

--- 5. Toggle Color Project/src/ToggleButtonColor.jsx
+++ 5. Toggle Color Project/src/ToggleButtonColor.jsx
@@ -5,11 +5,13 @@ const Main = () => {
   const [textColor, setTextColor] = useState("#1b1b1b");
   const [buttonStyle, setButtonStyle] = useState("white");
   const [headingText, setHeadingText] = useState("Welcome To The \n Real World \n 😇");
+
   function handleClick() {
     const isLightMode = backgroundColor === "white";
     setBackgroundColor(isLightMode ? "#1b1b1b" : "white");
     setTextColor(isLightMode ? "#ffa31a" : "#1b1b1b");
     setButtonStyle(isLightMode ? "#1b1b1b" : "white");
+    setHeadingText(isLightMode ? "Welcome To The \n Dark Side \n 🧛‍♂️" : "Welcome To The \n Real World \n 😇");
   }
 
   return (
@@ -18,6 +20,16 @@ const Main = () => {
         <button onClick={handleClick} style={{ buttonStyle, color: textColor, border: `5px solid ${textColor}` }}>
           {backgroundColor === "#1b1b1b" ? "Dark Mode" : "Light Mode"}
         </button>
+        <section>
+          <h1 className="heading">
+            {headingText.split("\n").map((line, index) => (
+              <span key={index}>
+                {line}
+                {index !== headingText.split("\n").length - 1 && <br />}
+              </span>
+            ))}
+          </h1>
+        </section>
       </section>
     </>
   );
