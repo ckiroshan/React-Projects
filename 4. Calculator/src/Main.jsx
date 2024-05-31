2adcb101a1190e779d928a6e3b4d3b3dc7347512
Your <ckiroshan@gmail.com>
2024-05-31 17:19:27 +0530

    Implemented display & clear functionality.

--- 4. Calculator/src/Main.jsx
+++ 4. Calculator/src/Main.jsx
@@ -3,11 +3,50 @@ import { useState } from "react";
 const Main = () => {
   const [inputValue, setInputValue] = useState("");
 
+  function display(value) {
+    setInputValue(inputValue + value);
+  }
+
+  function clear() {
+    setInputValue("");
+  }
+
   return (
     <>
       <h1 className="heading">Calculator App</h1>
       <form name="calc" className="calculator">
         <input type="text" className="value" value={inputValue} />
+        <span className="clear btn-grey" onClick={() => clear()}>
+          AC
+        </span>
+        <span onClick={() => display("%")} className="btn-grey">
+          %
+        </span>
+        <span onClick={() => display("/")} className="btn-orange">
+          /
+        </span>
+        <span onClick={() => display("7")}>7</span>
+        <span onClick={() => display("8")}>8</span>
+        <span onClick={() => display("9")}>9</span>
+        <span onClick={() => display("*")} className="btn-orange">
+          X
+        </span>
+        <span onClick={() => display("4")}>4</span>
+        <span onClick={() => display("5")}>5</span>
+        <span onClick={() => display("6")}>6</span>
+        <span onClick={() => display("-")} className="btn-orange">
+          -
+        </span>
+        <span onClick={() => display("1")}>1</span>
+        <span onClick={() => display("2")}>2</span>
+        <span onClick={() => display("3")}>3</span>
+        <span onClick={() => display("+")} className="btn-orange">
+          +
+        </span>
+        <span className="zero" onClick={() => display("0")}>
+          0
+        </span>
+        <span onClick={() => display(".")}>.</span>
       </form>
     </>
   );
