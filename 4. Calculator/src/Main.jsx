838361414b2f9e7f17117b2b0a30bdd608b6d8c2
Your <ckiroshan@gmail.com>
2024-05-31 13:05:14 +0530

    Intialized the Calculator project.

---
+++ 4. Calculator/src/Main.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const Main = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <>
+      <h1>Hello Calculator!</h1>
+    </>
+  );
+};
+
+export default Main;
