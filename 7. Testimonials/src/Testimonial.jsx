1062be6dc8f912fcda74fa1d3186b76b5fabf58d
Your <ckiroshan@gmail.com>
2024-06-01 17:42:52 +0530

    Added functionality to navigation buttons to switch testimonials.

--- 7. Testimonials/src/Testimonial.jsx
+++ 7. Testimonials/src/Testimonial.jsx
@@ -5,6 +5,14 @@ function Testimonial() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const testimonials = TestData;
 
+  const handlePrevClick = () => {
+    setCurrentIndex((currentIndex + testimonials.length - 1) % testimonials.length);
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((currentIndex + 1) % testimonials.length);
+  };
+
   return (
     <div className="container">
       <h1 className="heading">Testimonial Component</h1>
@@ -12,6 +20,10 @@ function Testimonial() {
         <div key={currentIndex} className="testimonials-content">
           <div className="testimonials-quote">" {testimonials[currentIndex].quote} "</div>
           <div className="testimonials-author">- {testimonials[currentIndex].author} -</div>
+        </div>
+        <div className="testimonials-nav">
+          <button onClick={handlePrevClick}>Prev</button>
+          <button onClick={handleNextClick}>Next</button>
         </div>
       </div>
     </div>
