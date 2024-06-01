f6218f4f5599d53b750e2342184a23f67f21b5b7
Your <ckiroshan@gmail.com>
2024-06-01 14:26:44 +0530

    Implemented handler for section onClick event.

--- 6. Hidden Search Bar/src/HiddenSearchBar.jsx
+++ 6. Hidden Search Bar/src/HiddenSearchBar.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
 const HiddenSearchBar = () => {
+  const [showInput, setShowInput] = useState(false);
+  const [bgColor, setBgColor] = useState("#f0f0f0");
+
+  const handleClick = (e) => {
+    if (e.target.className === "container") {
+      setShowInput(false);
+      setBgColor("#f0f0f0");
+    }
+  };
+
   return (
     <>
       <section onClick={handleClick} className="container" style={{ backgroundColor: bgColor }}>
