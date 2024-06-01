0112d741f838a0155c05d16fab22901987f465d6
Your <ckiroshan@gmail.com>
2024-06-01 14:24:32 +0530

    Added basic structure to HiddenSearchBar.

--- 6. Hidden Search Bar/src/HiddenSearchBar.jsx
+++ 6. Hidden Search Bar/src/HiddenSearchBar.jsx
@@ -1,7 +1,15 @@
 const HiddenSearchBar = () => {
   return (
     <>
+      <section onClick={handleClick} className="container" style={{ backgroundColor: bgColor }}>
+        {showInput ? (
+          <input type="text" placeholder="Just a searchbar!" autoFocus onBlur={() => setShowInput(false)} />
+        ) : (
+          <div className="icon-text-wrapper">
       <h1>Hey</h1>
+          </div>
+        )}
+      </section>
     </>
   );
 };
