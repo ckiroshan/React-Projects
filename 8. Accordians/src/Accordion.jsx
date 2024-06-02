7844800271671acf04267a5614accf5bac6c8bdd
Your <ckiroshan@gmail.com>
2024-06-02 10:51:39 +0530

    Implemented state logic for Accordion component.

--- 8. Accordians/src/Accordion.jsx
+++ 8. Accordians/src/Accordion.jsx
@@ -1,13 +1,14 @@
 import { useState, useRef, useEffect } from "react";
 
-const Accordion = () => {
-  
+const Accordion = ({ title, content }) => {
+  const [isActive, setIsActive] = useState(false);
+  const contentRef = useRef(null);
 
   return (
-    <section className={accordion-card}>
-      <div className="header">
+    <section className={`accordion-card ${isActive ? "active" : ""}`}>
+      <div className="header" onClick={() => setIsActive(!isActive)}>
         <div>{title}</div>
-        <p className="icon"></p>
+        <p className="icon">{isActive ? "-" : "+"}</p>
       </div>
       <div className="content">
         <p className="card-info"></p>
