5fa323e16583cbd8a9083f417f7c2a9a20d88329
Your <ckiroshan@gmail.com>
2024-06-02 10:45:42 +0530

    Updated Accordion with basic structure.

--- 8. Accordians/src/Accordion.jsx
+++ 8. Accordians/src/Accordion.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useState, useRef, useEffect } from "react";
 
 const Accordion = () => {
-  return (
-    <div>Accordion</div>
-  )
-}
+  
 
-export default Accordion
+  return (
+    <section className={accordion-card}>
+      <div className="header">
+        <div>{title}</div>
+        <p className="icon"></p>
+      </div>
+      <div className="content">
+        <p className="card-info"></p>
+      </div>
+    </section>
+  );
+};
+
+export default Accordion;
