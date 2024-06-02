638e9edc57624d177aa99b2a9c4ce5c91064fda8
Your <ckiroshan@gmail.com>
2024-06-02 10:38:03 +0530

    Intialized the Accordion Component.

---
+++ 8. Accordians/src/App.js
@@ -0,0 +1,14 @@
+import Accordion from "./Accordion";
+import accordionData from "./utils/content.js";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="heading">Accordion Component</h1>
+      <div className="accordion">
+      </div>
+    </div>
+  );
+};
+
+export default App;
