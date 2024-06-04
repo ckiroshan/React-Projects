18bee540bcb01b4b9555b90dfef52b9c1855248b
Your <ckiroshan@gmail.com>
2024-06-04 12:11:01 +0530

    Refactored Category, Colors & Price using Input component.

--- 10. Ecommerce - Filtering/src/Sidebar/Colors/Colors.jsx
+++ 10. Ecommerce - Filtering/src/Sidebar/Colors/Colors.jsx
@@ -1,33 +1,15 @@
+import Input from "../../components/Input";
 import "./Colors.css";
 
 const Colors = () => {
   return (
     <div>
       <h2 className="sidebar-title color-title">Color</h2>
-      <label className="sidebar-label-container color-title">
-        <input type="radio" name="test2" />
-        <span className="checkmark"></span>All
-      </label>
-      <label className="sidebar-label-container">
-        <input type="radio" name="test2" />
-        <span className="checkmark"></span>Black
-      </label>
-      <label className="sidebar-label-container">
-        <input type="radio" name="test2" />
-        <span className="checkmark"></span>Blue
-      </label>
-      <label className="sidebar-label-container">
-        <input type="radio" name="test2" />
-        <span className="checkmark"></span>Red
-      </label>
-      <label className="sidebar-label-container">
-        <input type="radio" name="test2" />
-        <span className="checkmark"></span>Green
-      </label>
-      <label className="sidebar-label-container">
-        <input type="radio" name="test2" />
-        <span className="checkmark"></span>White
-      </label>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </div>
   );
 };
---
+++ 10. Ecommerce - Filtering/src/components/Input.jsx
@@ -0,0 +1,10 @@
+const Input = () => {
+  return (
+    <label className="sidebar-label-container">
+      <input type="radio" name="test2" />
+      <span className="checkmark"></span>All
+    </label>
+  );
+};
+
+export default Input;
